fcf7bcf8b628907231eb6bf16d00d5832680004e
aabuiluk <andrii.a.builuk@gmail.com>
2024-01-24 20:24:45 +0200

    Lesson19

--- Lesson19.py
+++ Lesson19.py
@@ -8,8 +8,29 @@ driver.set_window_position(2000, 600)
 
 driver.maximize_window()
 
-url = "https://www.mensa.no/"
+url = "https://test.mensa.no/"
 
 driver.get(url)
 
-time.sleep(300)
+age1850_xpath = '//button[contains(@class,"ageselect_1850")]'
+age1850_button = driver.find_element(By.XPATH, age1850_xpath)
+age1850_button.click()
+time.sleep(3)
+
+start_id = 'startTest'
+start_button = driver.find_element(By.ID, start_id)
+start_button.click()
+time.sleep(3)
+
+q1_a_xpath = '//div[@id="question_0"]//div[@data-answerid="0"]'
+q1_a_button = driver.find_element(By.XPATH, q1_a_xpath)
+q1_a_button.click()
+time.sleep(3)
+
+q2_a_xpath = '//div[@id="question_1"]//div[@data-answerid="1"]'
+q2_a_button = driver.find_element(By.XPATH, q2_a_xpath)
+q2_a_button.click()
+time.sleep(3)
+
+
+time.sleep(3000)
